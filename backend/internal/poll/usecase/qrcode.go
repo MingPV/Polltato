be5f1c808f292d58ab37049abbee221ddb627467
Ball <yongsuwan.sb@gmail.com>
2026-04-17 21:41:28 +0700

    fix: update QR code URL path from /room/ to /poll/

--- backend/internal/poll/usecase/qrcode.go
+++ backend/internal/poll/usecase/qrcode.go
@@ -9,7 +9,7 @@ import (
 )
 
 func GenerateQRCodeBytes(roomID string) ([]byte, string, error) {
-	url := fmt.Sprintf("%s/room/%s", os.Getenv("FRONTEND_URL"), roomID)
+	url := fmt.Sprintf("%s/poll/%s", os.Getenv("FRONTEND_URL"), roomID)
 
 	png, err := qrcode.Encode(url, qrcode.Medium, 256)
 	if err != nil {
